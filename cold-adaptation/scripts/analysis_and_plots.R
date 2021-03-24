25bfdfe20cadac1931af77361efec26f0473f31a
Alice Chen <37118613+alicewchen@users.noreply.github.com>
2021-03-24 19:55:39 -0400

    changed y-axis title and plot colours

--- cold-adaptation/scripts/analysis_and_plots.R
+++ cold-adaptation/scripts/analysis_and_plots.R
@@ -4,20 +4,19 @@ library(tidyverse)
 library(dplyr)
 library(ggplot2)
 library(scales)
-library(ggpubr)
+library(gridExtra)
 library(MuMIn)
 library(car)
 load_theme_ltx()
-#####
-# Extract fluorescence data from gated .fcs files
-#####
 
+#=======================
+# Data manipulation
+#=======================
+# Extract fluorescence data from gated .fcs files ####
 #mean_PEA <- fcs_to_mean_PEA('../data/raw/fcs', '../data/clean/mean_PEA.txt')
 mean_PEA <- fcs_to_mean_logPEA('../data/raw/fcs', '../data/clean/mean_logPEA.txt')
 
-#####
-# Merge mean_PEA with treatment information using old clean dataframe
-#####
+# Merge mean_PEA with treatment information using old clean dataframe ####
 
 mean_PEA <- read.table("../data/clean/mean_logPEA.txt")
 ids <- read.table("../data/raw/Data.csv", sep = ",", header = TRUE) %>% 
@@ -57,58 +56,22 @@ df <- read.table("../data/raw/Data.csv", sep = ",", header = TRUE) %>%
       Latitude == "45.4" ~ "N",
       Latitude == "28.1" ~ "S"
       )))
-    #
-  
-#####
-# Dredge
-#####
 
-options(contrasts = c("contr.sum", "contr.poly")) #Set this option to run type 3 ANOVA
-lmdiff <- lm(mean_PE_A~ Latitude*Light*Timepoint*Temperature, data = df)
-lmdiff <- lmer(mean_PE_A ~ Latitude*Temperature+ Latitude*Light+ Light*Temperature+ (1|Strain) + (1|Timepoint), data = clean)
-summary(lmdiff)
-lmdiff_dredge<- 0
-options(na.action = "na.fail") #Set this option to run dredge
-lmdiff_dredge <- dredge(lmdiff, evaluate = TRUE, rank = "AICc")
-lmdiff_subset <- get.models(lmdiff_dredge, subset = delta < 2)
-lmdiff_avg <- model.avg(lmdiff_subset)
-summary(lmdiff_avg)
-options(na.action = "na.omit") #Set this option to restore to default option of R
 
-model <- lm(mean_PE_A ~ Latitude*Temperature + Latitude*Light+ Latitude*Timepoint + Light*Temperature + Light*Timepoint , data = df)
-aov<- Anova(model, type = 3)
-aov
-plot(model)
-qqnorm(resid(model))
-qqline(resid(model))
-hist(resid(model))
-
-#####
-# ANOVA
-#####
-
-library(car)
-model <- lmer(mean_PE_A ~ Latitude*Temperature+ Latitude*Light+ Light*Temperature+ (1|Strain) + (1|Timepoint) , data = clean)
-aov<- Anova(model, type = 3)
-aov
-plot(model)
-qqnorm(resid(model))
-qqline(resid(model))
-hist(resid(model))
-
-#remove outlier
+# Make a copy of dataframe without the outlier ####
 clean <- df %>% filter(!(mean_PE_A >= 58625))
 
-#check if random effects are needed
+#==========================
+# Linear model and ANOVA
+#==========================
+# check if random effects are needed ####
 model <- lm(mean_PE_A ~ Latitude*Temperature*Light*Strain*Timepoint, data = clean)
+aov<- Anova(model)
+aov 
+#Strain is a signficant term -> might need to set Strain as random effect
 
-#all interactions
-model <- lm(mean_PE_A ~ Latitude*Light+ Latitude*Timepoint+ Latitude*Temperature + Temperature*Light+ Temperature*Timepoint, data = clean)
-plot(model)
-aov<- Anova(model, type = 3)
-aov
 
-###Use this model (with outlier)
+# Linear model including all two-way interactions (with outlier)####
 model <- lmer(mean_PE_A ~ 
                 Latitude*Light+ 
                 Latitude*Timepoint+ 
@@ -129,7 +92,7 @@ qqline(resid(model))
 hist(resid(model))
 dev.off()
 
-#Same model as above without outlier
+# Linear model including all two-way interactions (without outlier) ####
 model <- lmer(mean_PE_A ~ 
                 Latitude*Light+ 
                 Latitude*Timepoint+ 
@@ -150,18 +113,6 @@ qqline(resid(model))
 hist(resid(model))
 dev.off()
 
-#Check equal variance among groups ####
-plot( model, resid(., type = "pearson") ~ fitted(.) | Latitude*Temperature,
-      id = 0.05, adj = -0.3 )
-plot( model, resid(., type = "pearson") ~ fitted(.) | Latitude*Light,
-      id = 0.05, adj = -0.3 )
-plot( model, resid(., type = "pearson") ~ fitted(.) | Light*Temperature,
-      id = 0.05, adj = -0.3 )
-plot( model, resid(., type = "pearson") ~ fitted(.) | Latitude,
-      id = 0.05, adj = -0.3 )
-#####
-
-
 ######
 # Plots
 ######
@@ -171,18 +122,22 @@ p[[1]]<-df %>% group_by(Latitude, Temperature) %>%
   ggplot(., aes(x = as.character(Latitude), y = mean, colour = Temperature)) +
   geom_point()+ 
   geom_line(aes(group = Temperature))+
-  geom_errorbar(aes(ymin= mean-se, ymax= mean+se), width = 0.1)+
+  geom_errorbar(aes(ymin= mean-se, ymax= mean+se), width = 0.05)+
   scale_y_continuous(labels = unit_format(unit = "K", scale = 0.001),
                      limits = c(12000, 30000)) +
-  xlab(expression("Latitude ("*degree*"N)")) +
-  ylab("Mean\nNR fluorescence") +
-  labs(color = "Temperature") +
+  scale_color_manual(labels = c(expression("4"*degree*"C"), expression("25"*degree*"C")), 
+                     values = c("#56B4E9","#E69F00"))+
+  labs(x=expression("Latitude ("*degree*"N)"),
+       y="NR fluorescence",
+       color = "Temperature",
+       tag="A")+
   theme_latex(base_size = 12) +
-  #scale_color_manual(values=c("#D55E00", "#0072B2")) +
   theme(axis.text = element_text(colour = "black"),
         axis.title.x = element_text(family = ),
         panel.grid.major.x = element_blank(),
-        panel.grid.major.y = element_blank())
+        panel.grid.major.y = element_blank(),
+        plot.tag = element_text(face = 'bold'),
+        legend.title = element_blank())
   
 
 p[[2]]<-df %>% group_by(Latitude, Light) %>%
@@ -191,18 +146,21 @@ p[[2]]<-df %>% group_by(Latitude, Light) %>%
   ggplot(., aes(x = as.character(Latitude), y = mean, color = Light)) +
   geom_point(position = position_dodge(width = 0.3))+ 
   geom_line(aes(group = Light),position = position_dodge(width = 0.3))+
-  geom_errorbar(aes(ymin= mean-se, ymax= mean+se), width = 0.1, position = position_dodge(width = 0.3))+
+  geom_errorbar(aes(ymin= mean-se, ymax= mean+se), width = 0.05, position = position_dodge(width = 0.3))+
   scale_y_continuous(labels = unit_format(unit = "K", scale = 0.001),
                      limits = c(12000, 30000)) +
-  xlab(expression("Latitude ("*degree*"N)")) +
-  ylab("Mean\nNR fluorescence") +
-  labs(color = "Light") +
+  scale_color_discrete(labels = c("No light", "Light"))+
+  labs(x=expression("Latitude ("*degree*"N)"),
+       y="NR fluorescence",
+       tag ="B",
+       colour = "Light")+
   theme_latex(base_size = 12) +
-  #scale_colour_manual(values=c("#D55E00", "#0072B2"))+
   theme(axis.text = element_text(colour = "black"),
         axis.title.x = element_text(family = ),
         panel.grid.major.x = element_blank(),
-        panel.grid.major.y = element_blank())
+        panel.grid.major.y = element_blank(),
+        plot.tag = element_text(face = 'bold'),
+        legend.title = element_blank())
 
 p[[3]]<-df %>% group_by(Latitude, Timepoint) %>%
   summarize(mean = mean(mean_PE_A),
@@ -210,11 +168,11 @@ p[[3]]<-df %>% group_by(Latitude, Timepoint) %>%
   ggplot(., aes(x = as.factor(Timepoint), y = mean, color = as.factor(Latitude))) +
   geom_point(position = position_dodge(width = 0.25))+ 
   geom_line(aes(group = Latitude),position = position_dodge(width = 0.25))+
-  geom_errorbar(aes(ymin= mean-se, ymax= mean+se), width = 0.1, position = position_dodge(width = 0.25))+
+  geom_errorbar(aes(ymin= mean-se, ymax= mean+se), width = 0.05, position = position_dodge(width = 0.25))+
   scale_y_continuous(labels = unit_format(unit = "K", scale = 0.001),
                      limits = c(12000, 30000)) +
   xlab("Hours of treatment") +
-  ylab("Mean\nNR fluorescence") +
+  ylab("NR fluorescence") +
   labs(color = "Latitude") +
   theme_latex(base_size = 12)+
   theme(axis.text = element_text(colour = "black"),
@@ -228,42 +186,68 @@ p[[4]]<-df %>% group_by(Light, Timepoint) %>%
   ggplot(., aes(x = as.factor(Timepoint), y = mean, color = Light)) +
   geom_point(position = position_dodge(width = 0.25))+ 
   geom_line(aes(group = Light),position = position_dodge(width = 0.25))+
-  geom_errorbar(aes(ymin= mean-se, ymax= mean+se), width = 0.1, position = position_dodge(width = 0.25))+
+  geom_errorbar(aes(ymin= mean-se, ymax= mean+se), width = 0.05, position = position_dodge(width = 0.25))+
   scale_y_continuous(labels = unit_format(unit = "K", scale = 0.001),
                      limits = c(12000, 30000)) +
-  xlab("Hours of treatment") +
-  ylab("Mean\nNR fluorescence") +
-  labs(color = "Light") +
+  #scale_color_discrete(labels = c("No light", "Light"))+
+  labs(x="Hours of treatment",
+       y="NR fluorescence",
+       #color ="Light",
+       tag = "C")+
   theme_latex(base_size = 12)+
   theme(axis.text = element_text(colour = "black"),
         axis.title.x = element_text(family = ),
         panel.grid.major.x = element_blank(),
-        panel.grid.major.y = element_blank())
+        panel.grid.major.y = element_blank(),
+        plot.tag = element_text(face = 'bold'),
+        legend.position = 'none')
 
 p[[5]]<-df %>% group_by(Light, Temperature) %>%
   summarize(mean = mean(mean_PE_A),
             se = sd(mean_PE_A)/sqrt(n())) %>%
   ggplot(., aes(x = as.factor(Temperature), y = mean, color = Light)) +
   geom_point(position = position_dodge(width = 0.25))+ 
-  #geom_line(aes(group = Light),position = position_dodge(width = 0.25))+
-  geom_errorbar(aes(ymin= mean-se, ymax= mean+se), width = 0.1, position = position_dodge(width = 0.25))+
+  geom_errorbar(aes(ymin= mean-se, ymax= mean+se), width = 0.05, position = position_dodge(width = 0.25))+
   scale_y_continuous(labels = unit_format(unit = "K", scale = 0.001),
                      limits = c(12000, 30000)) +
-  xlab(expression("Temperature ("*degree*"C)")) +
-  ylab("Mean\nNR fluorescence") +
-  labs(color = "Light") +
+  scale_color_discrete(labels = c("No light", "Light"))+
+  labs(x=expression("Temperature ("*degree*"C)"),
+       y="NR fluorescence",
+       colour="Light",
+       tag="D")+
   theme_latex(base_size = 12)+
   theme(axis.text = element_text(colour = "black"),
         axis.title.x = element_text(family = ),
         panel.grid.major.x = element_blank(),
-        panel.grid.major.y = element_blank())
+        panel.grid.major.y = element_blank(),
+        plot.tag = element_text(face = 'bold'),
+        legend.position = 'none')
 
+p[[6]]<-df %>% group_by(Temperature, Timepoint) %>%
+  summarize(mean = mean(mean_PE_A),
+            se = sd(mean_PE_A)/sqrt(n())) %>%
+  ggplot(., aes(x = as.factor(Timepoint), y = mean, color = Temperature)) +
+  geom_point(position = position_dodge(width = 0.25))+ 
+  geom_line(aes(group = Temperature),position = position_dodge(width = 0.25))+
+  geom_errorbar(aes(ymin= mean-se, ymax= mean+se), width = 0.05, position = position_dodge(width = 0.25))+
+  scale_y_continuous(labels = unit_format(unit = "K", scale = 0.001),
+                     limits = c(12000, 30000)) +
+  #scale_color_discrete(labels = c("No light", "Light"))+
+  labs(x="Hours of treatment",
+       y="NR fluorescence",
+       color ="Temperature",
+       tag = "E")+
+  theme_latex(base_size = 12)+
+  theme(axis.text = element_text(colour = "black"),
+        axis.title.x = element_text(family = ),
+        panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_blank(),
+        plot.tag = element_text(face = 'bold'))
 
-P <- ggarrange(p[[1]], p[[2]],p[[4]],p[[5]], labels = c("A","B","C","D"),
-                ncol = 2, nrow = 2)
+pdf("../analysis/interactions01.pdf", height = 6.5, width = 6.5)
 
-pdf("../analysis/interactions01.pdf", height = 8.5, width = 8.5)
-P
+P <- grid.arrange(p[[1]], p[[2]],p[[4]],p[[5]],
+                  ncol = 2, nrow = 2)
 dev.off()
 
 pdf("../analysis/interactions02.pdf", height = 6, width = 6)
